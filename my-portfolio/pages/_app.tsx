b0e203ad0864f759e887bbcf3d62250a0ee42036
Chaveen Dias <chaveend@gmail.com>
2025-03-14 08:14:25 +0530

    R

--- my-portfolio/pages/_app.tsx
+++ my-portfolio/pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { Sun, Moon, Menu, X } from "lucide-react";
 import "../styles/globals.css";
+import React from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
